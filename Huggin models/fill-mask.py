5c2ca228dbe2171c7d54a65398b7ce469df2f980
Marco Dalprato <5076292+MDalprato@users.noreply.github.com>
2024-07-22 18:33:41 +0200

    removed models

--- Huggin models/fill-mask.py
+++
@@ -1,4 +0,0 @@
-from transformers import pipeline
-unmasker = pipeline('fill-mask', model='distilroberta-base')
-out = unmasker("During a <mask> day the temperature drops and the wind increases.")
-print(out);
--- Huggin models/sentiment-analysis.py
+++
@@ -1,6 +0,0 @@
-from transformers import pipeline
-sentiment_pipeline = pipeline("sentiment-analysis")
-data = ["I love you", "I hate you", "my cat is sick"]
-sentiment_pipeline(data)
-
-print(sentiment_pipeline(data))
